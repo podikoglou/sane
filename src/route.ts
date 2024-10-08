59dd79080fa8a9d150dea97943b2ebcc0963de32
Alex <alex.podikoglou@gmail.com>
2024-10-08 23:17:12 +0300

    feat: post route type and creation method

--- src/route.ts
+++ src/route.ts
@@ -1,15 +1,21 @@
 import type { Action } from "./action.js";
 
 export type Route = {
-	path: string;
-	action: Action;
+  path: string;
+  action: Action;
 
-	// figure out a way to infer this
-	//controller: Controller
+  // figure out a way to infer this
+  //controller: Controller
 };
 
 export type GetRoute = Route & { method: "GET" };
 
 export function get(path: string, action: Action): GetRoute {
-	return { path, action, method: "GET" };
+  return { path, action, method: "GET" };
+}
+
+export type PostRoute = Route & { method: "POST" };
+
+export function post(path: string, action: Action): PostRoute {
+  return { path, action, method: "POST" };
 }
